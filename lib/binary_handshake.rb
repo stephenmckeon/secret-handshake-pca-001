0323bd786d122231bc7f3db81c6278e03f3068e0
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-09 21:55:05 -0400

    Shhhhh

--- lib/binary_handshake.rb
+++ lib/binary_handshake.rb
@@ -7,3 +7,25 @@
 # 10000 = Reverse the order of the operations in the secret handshake.
 
 # Write your code here.
+require "pry"
+
+
+class SecretHandshake
+  attr_reader :binary
+
+
+  def initialize(binary)
+    @binary = binary
+  end
+
+  def commands
+    array = @binary.split("")
+    secret_array = []
+    secret_array << "wink" if array.last == "1"
+    secret_array << "double blink" if array[-2] == "1"
+    secret_array << "close your eyes" if array[-3] == "1"
+    secret_array << "jump" if array[-4] == "1"
+    secret_array.reverse! if array[-5] == "1"
+    secret_array
+  end
+end
