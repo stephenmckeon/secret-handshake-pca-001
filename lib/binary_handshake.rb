8b53ae976233ff9e2a3a85e186e05345fa045874
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-09 21:57:25 -0400

    Shhhhh#2

--- lib/binary_handshake.rb
+++ lib/binary_handshake.rb
@@ -1,18 +1,5 @@
-# Binary Handshake Rules
-
-# 1 = "wink"
-# 10 = "double blink"
-# 100 = "close your eyes"
-# 1000 = "jump"
-# 10000 = Reverse the order of the operations in the secret handshake.
-
-# Write your code here.
-require "pry"
-
-
 class SecretHandshake
   attr_reader :binary
-
 
   def initialize(binary)
     @binary = binary
